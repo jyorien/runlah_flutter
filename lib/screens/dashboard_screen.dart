9d120c8ee77a924f3621487d65ae512230e941b2
joey <jo-_-ey@hotmail.com>
2021-05-07 21:11:52 +0800

    formatted date for card tile

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -2,8 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:runlah_flutter/components/card_tile.dart';
 import 'package:runlah_flutter/constants.dart';
+import 'package:intl/intl.dart';
 
 class DashboardScreen extends StatefulWidget {
 
@@ -47,13 +49,35 @@ class _DashboardScreenState extends State<DashboardScreen> {
         Expanded(
           child: ListView.builder(itemCount: dataList.length, itemBuilder: (context, index){
             var currentIndex = dataList[index];
-            double distance = currentIndex["distanceTravelled"];
+            double distance = (currentIndex["distanceTravelled"] / 1000);
             double speed = currentIndex["averageSpeed"];
+            Timestamp timestamp = currentIndex["timestamp"];
+            // convert timestamp to datetime and format date
+            DateTime date = timestamp.toDate().toLocal();
+            var dayOfMonth = DateFormat.d().format(date);
+            var monthName = DateFormat.MMMM().format(date);
+            var year = DateFormat.y().format(date);
+            var hour = DateFormat.H().format(date);
+            var minute = DateFormat.m().format(date);
+            var displayDate = "$dayOfMonth $monthName $year $hour:$minute";
+            // convert to map so it is iterable
+            var map = Map<String, dynamic>.from(currentIndex["coordinatesArray"]);
+            List<LatLng> listLatLng = [];
+            // get the latitude and longitude from the map to format into list of LatLng
+            map.forEach((key, value) {
+              var latitude = value["latitude"];
+              var longitude = value["longitude"];
+              listLatLng.add(LatLng(latitude, longitude));
+            });
+
             return CardTile(
               timeTaken: currentIndex["timeTaken"].toString(),
               sessionDistance: distance.toStringAsFixed(2),
               averageSpeed: speed.toStringAsFixed(2),
               stepCount: currentIndex["stepCount"].toString(),
+              dateTime: displayDate,
+              listLatLng: listLatLng,
+
 
             );
           })
