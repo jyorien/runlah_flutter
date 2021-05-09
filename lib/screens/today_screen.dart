9a1ea2a99d718667adc5e60525ea1d4ff092c587
joey <jyorin0206@gmail.com>
2021-05-09 21:07:23 +0800

    added temperature sensor

--- lib/screens/today_screen.dart
+++ lib/screens/today_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:environment_sensors/environment_sensors.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:runlah_flutter/constants.dart';
@@ -13,12 +14,48 @@ class TodayScreen extends StatefulWidget {
 class _TodayScreenState extends State<TodayScreen> {
   String _todayDistance = "0.00 km";
   String _todaySteps = "0 steps";
+  final environmentSensors = EnvironmentSensors();
+  Widget temperatureWidget = Container();
+  var temperatureStream;
+  String currentTemperature;
+
+  void displayTemperature() async {
+    await environmentSensors
+        .getSensorAvailable(SensorType.AmbientTemperature)
+        .then((isAvailable) {
+      if (isAvailable) {
+        initTemperatureStream();
+      }
+    });
+  }
+
+  void initTemperatureStream() {
+    temperatureStream = environmentSensors.temperature.listen((temperature) {
+      setState(() {
+        currentTemperature = temperature.toStringAsFixed(1);
+        temperatureWidget = Column(
+          children: [
+            Text('Temperature', style: kTodayTextStyle,),
+            Text("$currentTemperature \u2103", style: kTodayNumStyle,),
+          ],
+        );
+      } );
+    });
+  }
 
   @override
   void initState() {
     super.initState();
+    displayTemperature();
     getData();
   }
+
+  @override
+  void dispose() {
+    temperatureStream.dispose();
+    super.dispose();
+  }
+
   void getData() async {
     FirebaseFirestore firestore = FirebaseFirestore.instance;
     FirebaseAuth auth = FirebaseAuth.instance;
@@ -34,23 +71,29 @@ class _TodayScreenState extends State<TodayScreen> {
     result.docs.forEach((element) {
       Timestamp timestamp = element["timestamp"];
       var date = timestamp.toDate().toLocal();
-      if (date.day == today.day && date.month == today.month && date.year == today.year) {
+      if (date.day == today.day &&
+          date.month == today.month &&
+          date.year == today.year) {
         totalDistance += element["distanceTravelled"];
         totalSteps += element["stepCount"];
       }
-
     });
-    _todayDistance = "${(totalDistance/1000).toStringAsFixed(2)} km";
+    _todayDistance = "${(totalDistance / 1000).toStringAsFixed(2)} km";
     _todaySteps = "$totalSteps steps";
     setState(() {});
   }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Today's Distance", style: kTodayTextStyle,),
+          temperatureWidget,
+          Text(
+            "Today's Distance",
+            style: kTodayTextStyle,
+          ),
           Text(_todayDistance, style: kTodayNumStyle),
           Text("Today's Step Count", style: kTodayTextStyle),
           Text(_todaySteps, style: kTodayNumStyle),
